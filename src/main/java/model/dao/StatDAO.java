480608564e1ac0fff90a00422a5542f34e98bd11
HeoJiye <heoze1031@gmail.com>
2022-12-04 14:12:00 +0900

    stat dao/dto/manager 수정

--- src/main/java/model/dao/StatDAO.java
+++ src/main/java/model/dao/StatDAO.java
@@ -19,10 +19,9 @@ private JDBCUtil jdbcUtil = null;
 		
 	//stat정보 입력(하루 단위로 새로 생성)
 	public int create(StatDTO stat) throws SQLException {
-		String sql = "Insert Into Stat(userId, m_date, weight, kcal, carb, protein, fat) "
-					+ "Values (?, ?, ?, ?, ?, ?, ?) ";		
-		Object[] param = new Object[] {stat.getUserId(), stat.getDate(), stat.getWeight(), stat.getKcal(), 
-				stat.getCarb(), stat.getProtein(), stat.getFat()};				
+		String sql = "Insert Into Stat(userId, m_date, weight) "
+					+ "Values (?, ?, ?) ";		
+		Object[] param = new Object[] {stat.getUserId(), stat.getDate(), stat.getWeight()};				
 		jdbcUtil.setSqlAndParameters(sql, param);	// JDBCUtil 에 insert문과 매개 변수 설정
 						
 		try {				
@@ -41,10 +40,9 @@ private JDBCUtil jdbcUtil = null;
 	//기존의 stat 정보를 수정=식사 추가
 	public int update(StatDTO stat) throws SQLException {
 		String sql = "Update Stat "
-					+ "SET weight=?, kcal=?, carb=?. protein=?, fat=? "
-					+ "WHERE userid=? ";
-		Object[] param = new Object[] {stat.getWeight(), stat.getKcal(), stat.getCarb(), 
-				stat.getProtein(), stat.getFat(), stat.getUserId()};				
+					+ "SET weight=?"
+					+ "WHERE userid=? and m_date=?";
+		Object[] param = new Object[] {stat.getWeight(), stat.getUserId(), stat.getDate()};				
 		jdbcUtil.setSqlAndParameters(sql, param);	// JDBCUtil에 update문과 매개 변수 설정
 			
 		try {				
@@ -61,25 +59,24 @@ private JDBCUtil jdbcUtil = null;
 		return 0;
 	}
 	
-	// Nutri 정보가 있는 stat만 모아서 return
-	public List<StatDTO> nutriStatList(long userId) throws SQLException {
-        String sql = "SELECT kcal, carb, protein, fat "
+
+	// 사용자의 statList return
+	public List<StatDTO> statList(long userId) throws SQLException {
+		String sql = "SELECT m_date, weight "
         		+ " FROM stat "
         		+ "WHERE userId = ?";
-        Object[] param = new Object[] {userId};
+        Object[] param = new Object[] { userId };
 		jdbcUtil.setSqlAndParameters(sql, null);		// JDBCUtil에 query문 설정
 					
 		try {
 			ResultSet rs = jdbcUtil.executeQuery();			// query 실행			
 			List<StatDTO> nutriList = new ArrayList<StatDTO>();	
 			while (rs.next()) {
-				StatDTO nutri = new StatDTO(		
-					rs.getFloat("kcal"),
-					rs.getFloat("carb"),
-					rs.getFloat("protein"),
-					rs.getFloat("fat"));
-				nutriList.add(nutri);			
-			}		
+				StatDTO stat = new StatDTO();
+				stat.setDate(rs.getDate("date"));
+				stat.setWeight(rs.getFloat("weight"));
+				nutriList.add(stat);	
+			}
 			return nutriList;					
 			
 		} catch (Exception ex) {
@@ -89,59 +86,12 @@ private JDBCUtil jdbcUtil = null;
 		}
 		return null;
 	}
-	// weight 정보가 있는 stat만 모아서 return
-	public List<StatDTO> weightStatList(long userId) throws SQLException {
-		String sql = "SELECT weight "
-        		+ " FROM stat "
-        		+ "WHERE userId = ?";
-        Object[] param = new Object[] {userId};
-		jdbcUtil.setSqlAndParameters(sql, null);		// JDBCUtil에 query문 설정
-					
-		try {
-			ResultSet rs = jdbcUtil.executeQuery();			// query 실행			
-			List<StatDTO> nutriList = new ArrayList<StatDTO>();	
-			while (rs.next()) {
-				StatDTO nutri = new StatDTO(		
-					rs.getFloat("weight"));
-				nutriList.add(nutri);			
-			}		
-			return nutriList;					
-			
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		} finally {
-			jdbcUtil.close();		// resource 반환
-		}
-		return null;
-	}
-	
-	//addWeightStat용
-	public int addWeightStat(long userId, Date date, float weight) throws SQLException {
-		String sql = "Insert Into Stat( userId, m_date, weight ) "
-				+ "Values (?, ?, ?) ";
-		Object[] param = new Object[] { userId, date, weight };				
-		jdbcUtil.setSqlAndParameters(sql, param);	// JDBCUtil에 update문과 매개 변수 설정
-			
-		try {				
-			int result = jdbcUtil.executeUpdate();	// update 문 실행
-			return result;
-		} catch (Exception ex) {
-			jdbcUtil.rollback();
-			ex.printStackTrace();
-		}
-		finally {
-			jdbcUtil.commit();
-			jdbcUtil.close();	// resource 반환
-		}		
-		return 0;
-	}
-	
+
 	/**
 	 * 특정 날짜의 기록을 삭제(혹시몰라 넣어둠)
-	 
+	 */
 	public int delete(Date date) throws SQLException {
-		String sql = "Delete Fr
-		om Stat " 
+		String sql = "Delete From Stat " 
 					+ "Where m_Date = ? ";	
 		Object[] param = new Object[] { date };
 		jdbcUtil.setSqlAndParameters(sql, param);	// JDBCUtil에 delete문과 매개 변수 설정
@@ -159,5 +109,5 @@ private JDBCUtil jdbcUtil = null;
 		}		
 		return 0;
 	}
-	*/
+	
 }
--- src/main/java/model/dto/StatDTO.java
+++ src/main/java/model/dto/StatDTO.java
@@ -6,44 +6,16 @@ public class StatDTO {
 	private long userId;
 	private Date date;
 	private float weight;
-	private float kcal;
-	private float carb;
-	private float protein;
-	private float fat;
+
 	
 	public StatDTO() {} //기본 생성자
-	
-	//weightStatList용
-	public StatDTO(float weight) {
-		this.weight = weight;
-	}
-	//nutriStatList용
-	public StatDTO(float kcal, float carb, float protein, float fat) {
-		this.kcal = kcal;
-		this.carb = carb;
-		this.protein = protein;
-		this.fat = fat;
-	}
-	
-	public StatDTO(long userId, Date date, float weight, float kcal, float carb, float protein, float fat) {
+
+	public StatDTO(long userId, Date date, float weight) {
 		this.userId = userId;
 		this.date = date;
 		this.weight = weight;
-		this.kcal = kcal;
-		this.carb = carb;
-		this.protein = protein;
-		this.fat = fat;
 	}
 	
-	//update
-	public void update(StatDTO updateStat) {
-		this.weight = updateStat.weight;
-		this.kcal = updateStat.kcal;
-		this.carb = updateStat.carb;
-		this.protein = updateStat.protein;
-		this.fat = updateStat.fat;
-	}
-		
 	//getter & setter
 
 	public long getUserId() {
@@ -65,32 +37,4 @@ public class StatDTO {
 	public void setWeight(float weight) {
 		this.weight = weight;
 	}
-	public float getKcal() {
-		return kcal;
-	}
-	public void setKcal(float kcal) {
-		this.kcal = kcal;
-	}
-	public float getCarb() {
-		return carb;
-	}
-	public void setCarb(float carb) {
-		this.carb = carb;
-	}
-	public float getProtein() {
-		return protein;
-	}
-	public void setProtein(float protein) {
-		this.protein = protein;
-	}
-	public float getFat() {
-		return fat;
-	}
-	public void setFat(float fat) {
-		this.fat = fat;
-	}
-
-
-	
-	
 }
