3e362e6d6cb8fa4c75966f313921e76743ff4ea7
Fornamin <97093507+Fornamin@users.noreply.github.com>
2022-12-10 17:09:03 +0900

    reset

--- src/main/webapp/resources/js/home/home.js
+++ src/main/webapp/resources/js/home/home.js
@@ -55,15 +55,5 @@ function addFood() {
 	$modal_foodContainer.insertAdjacentHTML('beforeend', foodElement);
 	
 }
-
-$(document).ready(function() {
-	$('#exampleFormControlTextarea1').on('keyup', function() {
-		$('#test_cnt').html("("+$(this).val().length+" / 100)");
-							 
-		if($(this).val().length > 100) {
-			$(this).val($(this).val().substring(0, 100));
-			$('#test_cnt').html("(100 / 100)");
-		}
-	});
-});
+ 
 
