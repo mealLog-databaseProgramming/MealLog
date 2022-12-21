6cbb296347c8eb80d0e2deba3c808bc91e4880f2
HeoJiye <heoze1031@gmail.com>
2022-12-21 18:12:59 +0900

    Update groupEvents.js

--- src/main/webapp/resources/js/group/groupEvents.js
+++ src/main/webapp/resources/js/group/groupEvents.js
@@ -3,11 +3,11 @@ var body = document.querySelector('body');
 body.setAttribute('onLoad', 'init_group()');
 
 function init_group() {
-	init();
-	
 	groupCreate_init();
 	groupInfo_init();
 	groupView_init();
+	
+	init();
 }
 
 
