56e8340d73d038b14331044d1287228a9a0edc70
Fornamin <97093507+Fornamin@users.noreply.github.com>
2022-12-22 19:49:13 +0900

    구버전..

--- src/main/java/controller/home/ReactController.java
+++ src/main/java/controller/home/ReactController.java
@@ -16,50 +16,27 @@ import model.dto.ReplyDTO;
 import model.service.FeedManager;
 import model.service.ReactManager;
 
-// 하트 저장
-@ResponseBody
-@RequestMapping(value = "/newreact")
-public ReactDTO saveReact(@RequestParam long feedId, HttpSession session) {
-	long userId = UserSessionUtils.getLoginUserId(request.getSession());
-	ReactDTO react = new ReactDTO();
-	
-	react.setFeedId(feedId);
-	react.setUserId(userId);
-	react.setType('P');
-	
-	return react;
-}
+public class ReactController implements Controller {
 
-// 하트 해제
-@ResponseBody
-@RequestMapping(value = "/removereact") {
-public int removeReact(@RequestParam long feedId, HttpSession session) {
-	long userId = UserSessionUtils.getLoginUserId(request.getSession());
-	ReactManager man = new ReactManager();
-	
-	int result = man.remove(feedId, userId);
-	return result;
-}
-//public class ReactController implements Controller {
-	
-//	@Override
-//	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		ReactManager man = new ReactManager();
+		ReactDTO react = new ReactDTO(); // 임시
+	//	구현 실패...	
+		
+	//	Ajax에서 값 받아오기
+		
+		if (/*반응 없을 때*/) {
+		//  create React
+			man.create(react);
+		}
+		else {
+		// 	cancle React
+			man.remove(react);
+		}
+		 	
+		// redirect 하지 말고 ajax로..?
+		return "";
+	}
 
-		
-		
-		//		ReactManager man = new ReactManager();
-//		ReactDTO react = new ReactDTO(); // 임시
-//		
-//	//	Ajax에서 값 받아오기
-//		
-//		if (/*반응 없을 때*/) {
-//		//  create React
-//			man.create(react);
-//		}
-//		else {
-//		// 	cancle React
-//			man.remove(react);
-//		}
-//		 	
-//		// redirect 하지 말고 ajax로..?
-//		return "";
+}
